e8f94211a3bdcc9b7267add5bace6f1fd861bfbe
Alexander Yavtuhovich <yavtik@gmail.com>
2024-07-04 22:42:23 +0300

    (HW#4) added calculation modules and tests

---
+++ src/main/java/telran/introduce/Calculations.java
@@ -0,0 +1,44 @@
+package telran.introduce;
+
+public class Calculations {
+    public static int sum(int op1, int op2) {
+        return op1 + op2;
+    }
+
+    public static int multiply(int op1, int op2) {
+        return op1 * op2;
+    }
+
+    public static int divide(int op1, int op2) {
+        return op1 / op2;
+    }
+
+    public static int subtract(int op1, int op2) {
+        return op1 - op2;
+    }
+
+    public static int sumOfDigits(int number) {
+        int res = 0;
+        while (number > 0) {
+            res = res + number % 10;
+            number = number / 10;
+        }
+        return res;
+    }
+
+    public static int maxDigit(int number) {
+        int max = 0;
+        while (number > 0) {
+            if (max < number % 10) {
+                max = number % 10;
+            }
+            number = number / 10;
+        }
+        return max;
+    }
+
+    public static boolean isDividedOn(int number, int divider) {
+        return number%divider==0;
+    }
+
+}
---
+++ src/test/java/telran/introduce/CalculationsTest.java
@@ -0,0 +1,52 @@
+package telran.introduce;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import static telran.introduce.Calculations.divide;
+import static telran.introduce.Calculations.isDividedOn;
+import static telran.introduce.Calculations.maxDigit;
+import static telran.introduce.Calculations.multiply;
+import static telran.introduce.Calculations.subtract;
+import static telran.introduce.Calculations.sum;
+import static telran.introduce.Calculations.sumOfDigits;
+
+
+public class CalculationsTest {
+    @Test
+    void sumTest() {
+        assertEquals(4, sum(2, 2));
+    }
+
+    @Test
+    void multiplyTest() {
+        assertEquals(10, multiply(5, 2));
+    }
+
+    @Test
+    void divideTest() {
+        assertEquals(10, divide(20, 2));
+    }
+
+    @Test
+    void subtractTest() {
+        assertEquals(18, subtract(20, 2));
+    }
+
+    @Test
+    void sumOfDigitsTest() {
+        assertEquals(6, sumOfDigits(123));
+    }
+
+    @Test
+    void maxDigitTest() {
+        assertEquals(4, maxDigit(143));
+    }
+    @Test
+    void isDividedOnTest() {
+        assertEquals(true, isDividedOn(143,143));
+    }
+
+
+    
+}
