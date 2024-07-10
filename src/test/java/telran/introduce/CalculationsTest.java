24ed2b114f438fa1b886365c0723f6d96cab744f
Alexander Yavtuhovich <yavtik@gmail.com>
2024-07-10 14:32:58 +0300

    Fix tests

--- src/test/java/telran/introduce/CalculationsTest.java
+++ src/test/java/telran/introduce/CalculationsTest.java
@@ -1,13 +1,14 @@
 package telran.introduce;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import static telran.introduce.Calculations.divide;
 import static telran.introduce.Calculations.isDividedOn;
 import static telran.introduce.Calculations.maxDigit;
-import static telran.introduce.Calculations.multiply;
 import static telran.introduce.Calculations.subtract;
 import static telran.introduce.Calculations.sum;
 import static telran.introduce.Calculations.sumOfDigits;
@@ -17,36 +18,40 @@ public class CalculationsTest {
     @Test
     void sumTest() {
         assertEquals(4, sum(2, 2));
+        assertEquals(0, sum(2, -2));
+        assertEquals(-5, sum(-2, -3));
     }
-
-    @Test
-    void multiplyTest() {
-        assertEquals(10, multiply(5, 2));
-    }
-
-    @Test
-    void divideTest() {
-        assertEquals(10, divide(20, 2));
-        assertThrowsExactly(ArithmeticException.class, () -> divide(10, 0));
-    }
-
     @Test
     void subtractTest() {
-        assertEquals(18, subtract(20, 2));
+        assertEquals(0, subtract(2, 2));
+        assertEquals(4, subtract(2, -2));
+        assertEquals(1, subtract(-2, -3));
     }
-
+    @Test
+    void divideTest() {
+        assertEquals(1, divide(2, 2));
+        assertEquals(-1, divide(2, -2));
+        assertEquals(0, divide(-2, -3));
+        assertThrowsExactly(ArithmeticException.class, () -> divide(10, 0));
+    }
+    @Test
+    void maxDigitTest() {
+        assertEquals(3, maxDigit(-123));
+        assertEquals(3, maxDigit(123));
+        assertEquals(0, maxDigit(0));
+    }
     @Test
     void sumOfDigitsTest() {
         assertEquals(6, sumOfDigits(-123));
-    }
-
-    @Test
-    void maxDigitTest() {
-        assertEquals(4, maxDigit(-143));
+        assertEquals(6, sumOfDigits(123));
+        assertEquals(0, sumOfDigits(0));
     }
     @Test
     void isDividedOnTest() {
-        assertEquals(true, isDividedOn(143,143));
+        assertTrue(isDividedOn(10, 2));
+        assertTrue(isDividedOn(-10, 2));
+        assertFalse(isDividedOn(10, 0));
+        assertFalse(isDividedOn(10, 3));
     }
 
 
